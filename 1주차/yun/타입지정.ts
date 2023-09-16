a6af22fac5fcdd5faf3239d7f457917ea3620a74
YeonjunJung <duswns12893@gmail.com>
2023-09-16 21:12:09 +0900

    [Add] 타입지정, 타입추론 스크립트 파일 추가 및 내용 일부 정리

---
+++ 1주차/yun/타입지정.ts
@@ -0,0 +1,12 @@
+// string
+let text: string = 'hello';
+const sameText: string = 'Yeonjun';
+
+// number
+let score: number = 34.5;
+const sameScore: number = 123;
+
+// 매개변수 타입, 반환 타입 지정
+const plus = (a:number, b:number): number  => {
+    return a + b;
+}
